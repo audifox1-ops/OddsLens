2a1e7cda83ae1655d34a0addb002fd0333f32deb
audifox1-ops <audifox1@github.com>
2026-06-18 11:50:01 +0900

    chore: test vercel build using simple api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
 module.exports = function (req, res) {
-  try {
-    const app = require('../apps/server/dist/index.js').default;
-    return app(req, res);
-  } catch (err) {
-    console.error('🔥 Vercel Runtime Crash:', err);
-    res.status(500).json({
-      success: false,
-      error: {
-        code: 'RUNTIME_EVALUATION_CRASH',
-        message: err.message,
-        stack: err.stack,
-        retryable: false
-      }
-    });
-  }
+  res.status(200).json({ success: true, message: 'VERCEL_IS_WORKING' });
 };
